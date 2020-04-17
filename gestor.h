280b901aefa5694bc017d4716aa1ee539fbdb528
arctumn <arctumn@github.com>
2020-04-17 15:19:06 +0100

    update legibilidade

--- gestor.h
+++ gestor.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "gestorImplementation.h"
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -29,71 +30,10 @@ typedef struct processo{
     int processValue;   //valor do processo
 } processo;
 
-void atribuidorDeInstrucoes(char *nomeFich,char **arrayFinalStrings, processo *processoAtual);
+typedef struct programa
+{
+    processo infoProcesso;
+    char ** listaDeIntrucoes;
+}programa;
 
-void atribuidorDeInstrucoes(char *nomeFich,char **arrayFinalStrings, processo *processoAtual){ // DEVOLVER OQUE?
-    FILE *fp1 = fopen(nomeFich,"r");
-    char *arraydestrings[90];
-    char string[20];
-    int i = 0,c = 0,structChecker = 0;
-    if (fp1 == NULL){
-        perror("NAO EXISTE O FICHEIRO\n");
-        return;
-    }
-    while(fgets(string,sizeof(string),fp1) != 0){
-        arraydestrings[i] = strndup(string,sizeof(string));
-        printf("%s",arraydestrings[i]);
-        i++;
-    }
-    fclose(fp1);
-    i=0;
-    printf("\n--------------------\n");
-    while(arraydestrings[i] != NULL){
-        printf("%s",arraydestrings[i]);
-        if(!(strncmp(arraydestrings[i],"nome:",5))){
-            char *nome = strtok(arraydestrings[i],":");
-            processoAtual->nome = nome;
-            structChecker++; 
-        }
-        if(!(strncmp(arraydestrings[i],"processValue:",strlen("processValue:")))){
-            char *processValue = strtok(arraydestrings[i],"processValue:");
-            processoAtual->processValue = atoi(processValue);
-            structChecker++; 
-        }
-        if(!(strncmp(arraydestrings[i],"pid:",4))){
-            char *npid = strtok(arraydestrings[i],"pid:");
-            processoAtual->pid = atoi(npid);
-            structChecker++;
-        }
-        if(!(strncmp(arraydestrings[i],"ppid:",5))){
-            char *nppid = strtok(arraydestrings[i],"ppid:");
-            processoAtual->ppid = atoi(nppid);
-            structChecker++;
-        }
-        if(!(strncmp(arraydestrings[i],"prioridade:",strlen("prioridade:")))){
-            char *prioridade = strtok(arraydestrings[i],"prioridade:");
-            processoAtual->prioridade = atoi(prioridade);
-            structChecker++;
-        }
-       if(!(strncmp(arraydestrings[i],"tempoVida:",strlen("tempoVida:")))){
-            char *tVida = strtok(arraydestrings[i],"tempoVida:");
-            processoAtual->tempoVida = atoi(tVida);
-            structChecker++; 
-        } 
-        if(!(strncmp(arraydestrings[i],"PC:",strlen("PC:")))){
-            char *PC = strtok(arraydestrings[i],"PC:");
-            processoAtual->PC = atoi(PC);
-            structChecker++;
-        }
-        if(!(strncmp(arraydestrings[i],"I:",strlen("I:")))){
-            char *I = strtok(arraydestrings[i],"I:");
-            arrayFinalStrings[c] = I;
-            c++;
-        }
-        i++;
-    }
-    if (structChecker != 7){
-        printf("Programa Invalido\n");
-        return;
-    }
-}
+void atribuidorDeInstrucoes(char *nomeFich,char **arrayFinalStrings, processo *processoAtual);
